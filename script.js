f23062e107015837a3f3e4ecedbc42a643302b17
Luis Mario <luismario.lr46@gmail.com>
2023-04-25 00:10:14 -0700

    Design

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ onload = (event) => {
   agregarRestriccion();
   agregarRestriccion();
   document.getElementById("funcion-objetivo").value = "40x+50y";
-  document.getElementById("restriccion1").value = "12 5x+200y<=5000";
+  document.getElementById("restriccion1").value = "125x+200y<=5000";
   document.getElementById("restriccion2").value = "150x+100y<=3000";
   document.getElementById("restriccion3").value = "67x+25y<=1000";
 }
+
+
+
+
 let letrasMayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 agregarRestriccion = () => {
   var restricciones = document.getElementById("restricciones");
@@ -15,9 +19,18 @@ agregarRestriccion = () => {
       <label for="restriccion${numRestricciones}">
         Restricción ${numRestricciones}:
       </label>
-      <input type="text" id="restriccion${numRestricciones}" name="restriccion${numRestricciones}">
+      <input type="text" id="restriccion${numRestricciones}" class="" name="restriccion${numRestricciones}">
     `;
   restricciones.appendChild(nuevaRestriccion);
+}
+quitarRestriccion = () => {
+
+  let restricciones = document.getElementById("restricciones");
+  let ultimo = restricciones.lastElementChild;
+  //console.log(restricciones.childElementCount)
+  if (restricciones.childElementCount >= 3)
+    restricciones.removeChild(ultimo);
+
 }
 calcularPuntos = (ecuacion) => {
   // Separar los coeficientes y el término constante
@@ -38,6 +51,12 @@ convertirIgual = (restriccion = "") => {
   else console.log("error inecuacion");
 }
 graficar = () => {
+  document.getElementById("aviso").innerHTML = "";
+  document.getElementById("todo").hidden = true;
+  if (validar()) document.getElementById("todo").hidden = false;
+  else {
+    avisoError("Hubo un error en los datos"); return false;
+  }
   var objetivo = parseInt(document.getElementById("objetivo").value);
   var restriccionesInputs = document.querySelectorAll("#restricciones input");
   var restricciones = [];
@@ -53,39 +72,43 @@ graficar = () => {
   arregloInecuaciones.push("y>=0");
   var ggbApp = new GGBApplet({
     "appName": "classic",
-    //"material_id": "mwH6n8f6",
-    "width": 800,
+    "width": 600,
     "height": 600,
     "showToolBar": false,
-    "showMenuBar": false, 
+    "showMenuBar": false,
     "showAlgebraInput": false,
-    "allowUpscale": true,
-    "enableLabelDrags": true,
-    "enableShiftDragZoom": true,
-    "capturingThreshold": null,
-    "borderColor": null,
-    "preventFocus": false,
-    "useBrowserForJS": false,
-    "isUnbundled": false,
-    "rounding": null,
-    "errorDialogsActive": true,
-    "synchronizationURL": null,
-    "synchronizationInterval": null,
-    "synchronizationTimeout": null,
-    "enableUndoRedo": true,
-    "enableRightClick": true,
-    "showAnimationButton":false,
-    "showFullscreenButton":false,
-    "showSuggestionButtons":false,
+    "allowUpscale": false,
+    // "enableLabelDrags": true,
+    // "enableShiftDragZoom": true,
+    // "capturingThreshold": null,
+    // "borderColor": null,
+    // "preventFocus": false,
+    // "useBrowserForJS": false,
+    // "isUnbundled": false,
+    // "rounding": null,
+    // "errorDialogsActive": true,
+    // "synchronizationURL": null,
+    // "synchronizationInterval": null,
+    // "synchronizationTimeout": null,
+    // "enableUndoRedo": true,
+    // "enableRightClick": true,
+    // "showAnimationButton":false,
+    // "showFullscreenButton":false,
+    // "showSuggestionButtons":false,
     appletOnLoad(api) {
       api.setRounding("5");
       api.evalCommand(codigoGeoGebra);
+      api.setGridVisible(1);
+      //api.writePNGtoFile("SoluciÃ³n.png", 1, !1, 144);
       //api.evalCommand("Punto = Vertex(regionFactible)");
       //api.evalCommand("Unique(Vertex(regionFactible))");
+      let flag = true;
       let listaPuntosRegionFactible = [];
       let nombrePuntos = api.evalCommandGetLabels("Punto = Vertex(regionFactible)").split(",");
-
-      //console.log(nombrePuntos);
+      if (nombrePuntos.length === 1) {
+        avisoError("No hay puntos para trazar una region factible");
+        flag = false;
+      }//console.log(nombrePuntos);
       //console.log(api.getValueString(nombrePuntos[0]));
       //let valor = api.evalCommandCAS(nombrePuntos[0]);
       for (let i = 0; i < nombrePuntos.length; i++) {
@@ -117,45 +140,64 @@ graficar = () => {
         codigo += "\n";
       }
       api.evalCommand(codigo);
-
+      for (count = 0; count < puntosConLasMismasCoordenadas.length; count++) {
+        api.setCaption(`${letrasMayusculas[count]}`, "Hola");
+        api.setColor(`${letrasMayusculas[count]}`, 0, 0, 0);
+        api.setLabelStyle(`${letrasMayusculas[count]}`, 1);
+        api.setLayer(`${letrasMayusculas[count]}`, 9);
+        api.setLabelVisible(`${letrasMayusculas[count]}`, !0)
+      }
       //api.setVisible("A",false);
       for (let i = 0; i < c; i++) {
-        api.setVisible(`Punto${i+1}`,false);
-        if(i%2==0)api.setVisible(`Seg${i+1}y${i+2}`,false);
+        api.setVisible(`Punto${i + 1}`, false);
+        // let x = api.getXcoord(`Punto${i + 1}`);
+        // let y = api.getYcoord(`Punto${i + 1}`);
+
+        // api.setCaption(`Punto${i + 1}`,`(${x},${y})`);
+        if (i % 2 == 0) {
+          api.setVisible(`Seg${i + 1}y${i + 2}`, true);
+          api.setCaption(`Seg${i + 1}y${i + 2}`, `Restriccion ${(i / 2) + 1}`);
+          api.setLabelVisible(`Seg${i + 1}y${i + 2}`, true);
+          api.setLabelStyle(`Seg${i + 1}y${i + 2}`, 3);
+          api.setLayer(`Seg${i + 1}y${i + 2}`, 0);
+        }
       }
-      api.setLabelVisible('regionFactible',false);
+      api.setLabelVisible('regionFactible', false);
+      api.setTextValue('regionFactible', "Region factible");
       //api.setPointSize('A',8);
       let funcion_objetivo = document.getElementById("funcion-objetivo").value;
       let coeficienteX = 0;
       let coeficienteY = 0;
       let signo = '';
-      if(funcion_objetivo.includes('+'))
-      {
+      if (funcion_objetivo.includes('+')) {
         signo = '+';
         coeficienteX = funcion_objetivo.split('+')[0].replace(/[^0-9.]+/g, "");
         coeficienteY = funcion_objetivo.split('+')[1].replace(/[^0-9.]+/g, "");
-      }else if(funcion_objetivo.includes('-')){
+      } else if (funcion_objetivo.includes('-')) {
         signo = '-';
         coeficienteX = funcion_objetivo.split('-')[0].replace(/[^0-9.]+/g, "");
         coeficienteY = funcion_objetivo.split('-')[1].replace(/[^0-9.]+/g, "");
-      }else console.log("error en coeficiente F O");
+      } else console.log("error en coeficiente F O");
+
+
+
+
       let evaluacion = document.createElement("div");
       evaluacion.innerHTML = "<h2>Encontrando la solucion optima</h2>";
       evaluacion.innerHTML += `<h2>Funcion objetivo: <u>${funcion_objetivo}</u></h2>`;
       evaluacion.innerHTML += "<p>";
       let valor = [];
       let can = 0;
-      for(let i = 0;i < puntosConLasMismasCoordenadas.length;i++)
-      {
-        signo=='+'? can = Number((coeficienteX*puntosConLasMismasCoordenadas[i].x)+(coeficienteY*puntosConLasMismasCoordenadas[i].y)).toFixed(2):can =Number((coeficienteX*puntosConLasMismasCoordenadas[i].x)-(coeficienteY*puntosConLasMismasCoordenadas[i].y)).toFixed(2);
-        valor.push({"index":i+1,"cantidad":can});
-        evaluacion.innerHTML+=`
+      for (let i = 0; i < puntosConLasMismasCoordenadas.length; i++) {
+        signo == '+' ? can = Number((coeficienteX * puntosConLasMismasCoordenadas[i].x) + (coeficienteY * puntosConLasMismasCoordenadas[i].y)).toFixed(2) : can = Number((coeficienteX * puntosConLasMismasCoordenadas[i].x) - (coeficienteY * puntosConLasMismasCoordenadas[i].y)).toFixed(2);
+        valor.push({ "letra": String.fromCharCode(64 + i + 1), "x": puntosConLasMismasCoordenadas[i].x, "y": puntosConLasMismasCoordenadas[i].y, "index": i + 1, "cantidad": can });
+        evaluacion.innerHTML += `
         ${letrasMayusculas[i]}(${puntosConLasMismasCoordenadas[i].x},
           ${puntosConLasMismasCoordenadas[i].y}) = ${coeficienteX}(${puntosConLasMismasCoordenadas[i].x})${signo}${coeficienteY}(${puntosConLasMismasCoordenadas[i].y}) = ${valor[i].cantidad}`;
-        evaluacion.innerHTML+="<br>";
+        evaluacion.innerHTML += "<br>";
       }
-      evaluacion.innerHTML+="</p>"
-      let maximo = Math.max(...(valor.map(valor=>parseFloat(valor.cantidad))));
+      evaluacion.innerHTML += "</p>"
+      let maximo = Math.max(...(valor.map(valor => parseFloat(valor.cantidad))));
       const registro_mayor_cantidad = valor.reduce((max, obj) => {
         const cantidad = parseFloat(obj.cantidad);
         return cantidad > max.cantidad ? { ...obj } : { ...max };
@@ -164,30 +206,151 @@ graficar = () => {
         const cantidad = parseFloat(obj.cantidad);
         return cantidad < min.cantidad ? { ...obj } : { ...min };
       });
+      console.log(valor);
+
+
+
+
+
+
+      // Obtener el elemento contenedor
+      const contenedor = document.getElementById('tabla');
+      contenedor.innerHTML = "";
+      // Crear el elemento tabla
+      const tabla = document.createElement('table');
+      tabla.classList.add('table', 'table-striped');
+
+      // Crear el elemento cabecera
+      const cabecera = document.createElement('thead');
+      const filaCabecera = document.createElement('tr');
+
+      // Crear las celdas de la cabecera
+      const celdaPunto = document.createElement('th');
+      celdaPunto.textContent = 'Punto';
+
+      const celdaCoordenadas = document.createElement('th');
+      celdaCoordenadas.textContent = 'Coordenadas(X,Y)';
+
+      const celdaEvaluacion = document.createElement('th');
+      celdaEvaluacion.textContent = `Evaluacion con la funcion objetivo z = ${funcion_objetivo}`;
+
+      // Agregar las celdas a la fila de la cabecera
+      filaCabecera.appendChild(celdaPunto);
+      filaCabecera.appendChild(celdaCoordenadas);
+      filaCabecera.appendChild(celdaEvaluacion);
+
+      // Agregar la fila de la cabecera al elemento cabecera
+      cabecera.appendChild(filaCabecera);
+
+      // Agregar la cabecera a la tabla
+      tabla.appendChild(cabecera);
+
+      // Agregar la tabla al contenedor
+      contenedor.appendChild(tabla);
+      // Obtener el elemento cuerpo de la tabla
+      const cuerpo = document.createElement('tbody');
+
+      // Recorrer los datos y crear las filas y celdas correspondientes
+      valor.forEach((dato, indice) => {
+        const fila = document.createElement('tr');
+
+        const celdaPunto = document.createElement('td');
+        celdaPunto.textContent = dato.letra;
+
+        const celdaCoordenadas = document.createElement('td');
+        celdaCoordenadas.textContent = `${dato.x}, ${dato.y}`;
+
+        const celdaEvaluacion = document.createElement('td');
+        celdaEvaluacion.textContent = `${coeficienteX}(${Number(valor[indice].x)}) ${signo == '+' ? '+' : '-'}${coeficienteY}(${valor[indice].y}) = ${(signo == '+' ? coeficienteX * valor[indice].x + coeficienteY * valor[indice].y : coeficienteX * valor[indice].x - coeficienteY * valor[indice].y).toFixed(2)}`;
+
+        // Agregar las celdas a la fila
+        fila.appendChild(celdaPunto);
+        fila.appendChild(celdaCoordenadas);
+        fila.appendChild(celdaEvaluacion);
+
+        // Agregar la fila al cuerpo de la tabla
+        cuerpo.appendChild(fila);
+      });
+
+      // Obtener la tabla y agregar el cuerpo
+
+      tabla.appendChild(cuerpo);
+      /*
+      <div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Well done!</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+  <hr>
+  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+</div>
       
-      //console.log(registro_mayor_cantidad);
-      //console.log(registro_menor_cantidad);
-      if(objetivo===1)
-      {
-        api.setColor(letrasMayusculas[registro_mayor_cantidad.index-1],255,0,0);
-        api.setPointSize(letrasMayusculas[registro_mayor_cantidad.index-1],9);
-        evaluacion.innerHTML+= `<h3>Solucion optima: x = ${puntosConLasMismasCoordenadas[registro_mayor_cantidad.index-1].x}, y = ${puntosConLasMismasCoordenadas[registro_mayor_cantidad.index-1].y} = ${registro_mayor_cantidad.cantidad}</h3>`;
+      */
+      let alerta = document.createElement("div");
+      alerta.classList.add("alert", "alert-success");
+      let header4 = document.createElement("h4");
+      header4.textContent = "Solucion optima";
+      alerta.appendChild(header4);
+      let parr = document.createElement("p");
+      let contexto = document.createElement("p");
+      let arreglo = [];
+      console.log(registro_mayor_cantidad, registro_menor_cantidad, signo);
+      if (objetivo==1) {
+        arreglo.push(valor[registro_mayor_cantidad.index - 1].x)
+        arreglo.push(valor[registro_mayor_cantidad.index - 1].y);
+        if(signo == '+')
+        parr.textContent = `La solucion optima es Z = ${(coeficienteX * arreglo[0] + coeficienteY * arreglo[1]).toFixed(2)}`;
+        else
+        parr.textContent = `La solucion optima es Z = ${(coeficienteX * arreglo[0] - coeficienteY * arreglo[1]).toFixed(2)}`;
+
+      } else {
+        arreglo.push(valor[registro_menor_cantidad.index - 1].x)
+        arreglo.push(valor[registro_menor_cantidad.index - 1].y);
+        if(signo == '-')
+        parr.textContent = `La solucion optima es Z = ${(coeficienteX * arreglo[0] - coeficienteY * arreglo[1]).toFixed(2)}`;
+        else
+        parr.textContent = `La solucion optima es Z = ${(coeficienteX * arreglo[0] + coeficienteY * arreglo[1]).toFixed(2)}`;
+      }
+      alerta.appendChild(parr);
+      alerta.appendChild(document.createElement("hr"));
+      contexto.textContent = `Se obtiene con los valores X = ${arreglo[0]} y Y = ${arreglo[1]}`;
+      alerta.appendChild(contexto);
+      contenedor.appendChild(alerta);
+
+
+
+
+
+
+
+
+
+
+
+
+
+      if (objetivo === 1) {
+        api.setColor(letrasMayusculas[registro_mayor_cantidad.index - 1], 255, 0, 0);
+        api.setPointSize(letrasMayusculas[registro_mayor_cantidad.index - 1], 9);
+        api.setLineThickness(letrasMayusculas[registro_mayor_cantidad.index - 1], 10);
+        marcarFila(registro_mayor_cantidad.index - 1);
+        evaluacion.innerHTML += `<h3>Solucion optima: x = ${puntosConLasMismasCoordenadas[registro_mayor_cantidad.index - 1].x}, y = ${puntosConLasMismasCoordenadas[registro_mayor_cantidad.index - 1].y} = ${registro_mayor_cantidad.cantidad}</h3>`;
 
       }
-      else if(objetivo===2)
-      {
-        api.setColor(letrasMayusculas[registro_menor_cantidad.index-1],255,0,0);
-        api.setPointSize(letrasMayusculas[registro_menor_cantidad.index-1],9);
-        evaluacion.innerHTML+= `<h3>Solucion optima: x = ${puntosConLasMismasCoordenadas[registro_menor_cantidad.index-1].x}, y = ${puntosConLasMismasCoordenadas[registro_menor_cantidad.index-1].y} = ${registro_menor_cantidad.cantidad}</h3>`;
+      else if (objetivo === 2) {
+        api.setColor(letrasMayusculas[registro_menor_cantidad.index - 1], 255, 0, 0);
+        api.setPointSize(letrasMayusculas[registro_menor_cantidad.index - 1], 9);
+        api.setLineThickness(letrasMayusculas[registro_menor_cantidad.index - 1], 10);
+        marcarFila(registro_menor_cantidad.index - 1);
+        evaluacion.innerHTML += `<h3>Solucion optima: x = ${puntosConLasMismasCoordenadas[registro_menor_cantidad.index - 1].x}, y = ${puntosConLasMismasCoordenadas[registro_menor_cantidad.index - 1].y} = ${registro_menor_cantidad.cantidad}</h3>`;
 
       }
-      else
-      {
+      else {
 
       }
       document.getElementById("evaluacion").innerHTML = "";
-      document.getElementById("evaluacion").appendChild(evaluacion);
-      
+      if (flag)
+        document.getElementById("tabla").hidden = false;
+      else document.getElementById("tabla").hidden = true;
+
       //Color
       //api.setColor('A',255,0,0);
       //api.setLineStyle("A",3);
@@ -199,12 +362,7 @@ graficar = () => {
 
   restricciones.forEach((value, index) => {
 
-    // codigoGeoGebra += `${letrasMayusculas[c]} = (x=${restricciones[index][0].x}, y = ${restricciones[index][0].y})`;
-    // codigoGeoGebra += "\n";
-    // codigoGeoGebra += `${letrasMayusculas[c + 1]} = (x=${restricciones[index][1].x}, y = ${restricciones[index][1].y})`;
-    // codigoGeoGebra += "\n";
-    // codigoGeoGebra += `Seg${letrasMayusculas[c]}${letrasMayusculas[c + 1]} = Segment(${letrasMayusculas[c]},${letrasMayusculas[c + 1]})`;
-    // codigoGeoGebra += "\n";
+
     codigoGeoGebra += `Punto${c + 1} = Intersect(x=${restricciones[index][0].x}, y = ${restricciones[index][0].y})`;
     codigoGeoGebra += "\n";
     codigoGeoGebra += `Punto${c + 2} = Intersect(x=${restricciones[index][1].x}, y = ${restricciones[index][1].y})`;
@@ -212,7 +370,7 @@ graficar = () => {
     codigoGeoGebra += `Seg${c + 1}y${c + 2} = Segment(Punto${c + 1},Punto${c + 2})`;
     codigoGeoGebra += "\n";
 
-    //∧
+
     c += 2;
   });
   codigoGeoGebra += "regionFactible = ("
@@ -251,5 +409,40 @@ function encontrarCoincidencias(array) {
 
   return JSON.stringify(puntosFinales);
 }
+avisoError = (mensaje = "") => {
+  let aviso = document.createElement("div");
+  aviso.role = "alert";
+  aviso.classList.add("alert");
+  aviso.classList.add("alert-danger");
 
-
+  aviso.innerHTML = mensaje;
+  let DOMaviso = document.getElementById("aviso");
+  DOMaviso.innerHTML = '';
+  DOMaviso.appendChild(aviso);
+}
+validar = () => {
+  let totalRestricciones = document.getElementById("restricciones").childElementCount;
+  for (let i = 1; i < totalRestricciones; i++) {
+    if (document.getElementById(`restriccion${i}`).value == "") return false;
+    if (!reconocerInecuacion(document.getElementById(`restriccion${i}`).value)) return false;
+  }
+  return true;
+}
+reconocerInecuacion = (str) => {
+  //var re = /^(-?\d*\.?\d+|\d*[a-z]|\d+)\s*[+]\s*(-?\d*\.?\d+|\d*[a-z]|\d+)\s*([<>]?=)\s*(-?\d*\.?\d+)$/;
+  let re = /^\d*x\s*[+-]\s*\d*y\s*(?:>=|<=)\s*\d+$/;
+  return re.test(str);
+}
+limpiar = () => {
+  let totalRestricciones = document.getElementById("restricciones").childElementCount;
+  for (let i = 1; i < totalRestricciones; i++) {
+    document.getElementById(`restriccion${i}`).value = "";
+  }
+  document.getElementById("funcion-objetivo").value = "";
+  document.getElementById("objetivo").value = "1";
+  document.getElementById("todo").hidden = true;
+}
+marcarFila = (index) => {
+  const filas = document.querySelectorAll('tbody tr');
+  filas[index].classList.add('marcada');
+}
